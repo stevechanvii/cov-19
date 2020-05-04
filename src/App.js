b7fd59d20237ce6bc8b782a890ed193627480134
Steve Chan <dche0007@student.monash.edu>
2020-05-04 17:17:11 +1000

    implement data on chart

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import Cards from './components/Cards/Cards';
 import Chart from './components/Chart/Chart';
 import CountryPicker from './components/CountryPicker/CountryPicker';
 import styles from './App.module.css';
+import { fetchData } from './api';
 
 class App extends React.Component {
+    state = {
+        data: {},
+    };
+
+    async componentDidMount() {
+        const covData = await fetchData();
+        this.setState({ data: covData });
+    }
+
     render() {
+        const { data } = this.state;
+
         return (
             <div className={styles.container}>
-                <Cards />
+                <Cards data={data} />
                 <Chart />
                 <CountryPicker />
             </div>
