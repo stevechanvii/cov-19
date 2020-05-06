9c8d70629e522534ad6b51b57cf633ea57e20afe
Steve Chan <dche0007@student.monash.edu>
2020-05-06 18:07:42 +1000

    Updates

---
+++ precache-manifest.4be42fc319688b77c41bc957546d5237.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6f21860c0d3da70476913524578a7c",
+    "url": "/cov-19/index.html"
+  },
+  {
+    "revision": "bf20323bd825a18caacb",
+    "url": "/cov-19/static/css/main.41676c8b.chunk.css"
+  },
+  {
+    "revision": "1f4deb637160e84ca2b6",
+    "url": "/cov-19/static/js/2.2d816165.chunk.js"
+  },
+  {
+    "revision": "1fd4b1d005d715d5cbab4fdcc335be90",
+    "url": "/cov-19/static/js/2.2d816165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf20323bd825a18caacb",
+    "url": "/cov-19/static/js/main.0b152749.chunk.js"
+  },
+  {
+    "revision": "f5b64447a08d4ebcde50",
+    "url": "/cov-19/static/js/runtime-main.32319de5.js"
+  }
+]);
