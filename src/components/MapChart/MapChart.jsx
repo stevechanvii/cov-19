88971ee0833ae30ed6d1abd4a75f9d9e07fe9031
Steve Chan <dche0007@student.monash.edu>
2020-05-06 18:22:35 +1000

    fixed map

--- src/components/MapChart/MapChart.jsx
+++ src/components/MapChart/MapChart.jsx
@@ -24,51 +24,49 @@ const MapChart = ({ countryHovered, handleCountryChange }) => {
                 data-tip=''
                 projectionConfig={{ scale: 100, rotate: [-160, 0, 0] }}
             >
-                <ZoomableGroup>
-                    <Geographies geography={geoGlobal}>
-                        {({ geographies }) =>
-                            geographies.map((geo) => (
-                                <Geography
-                                    key={geo.rsmKey}
-                                    geography={geo}
-                                    stroke='#EAEAEC'
-                                    strokeWidth='0.5'
-                                    onMouseEnter={() => {
-                                        const { NAME } = geo.properties;
-                                        countryHovered(`${NAME}`);
-                                    }}
-                                    onMouseLeave={() => {
-                                        countryHovered('');
-                                    }}
-                                    onClick={() => onMapClicked(geo.properties)}
-                                    fill={
-                                        countrySelected === geo.properties.NAME
-                                            ? '#668cff'
-                                            : '#9998A3'
-                                    }
-                                    style={{
-                                        default: {
-                                            outline: 'none',
-                                        },
-                                        hover: {
-                                            fill: '#809fff',
-                                            stroke: '#3366ff',
-                                            strockWidth: 0.5,
-                                            outline: 'none',
-                                        },
-                                        pressed: {
-                                            fill: '#DD4132',
-                                            stroke: '#9E1030',
-                                            strokeWidth: 0.75,
-                                            outline: 'none',
-                                            transition: 'all 250ms',
-                                        },
-                                    }}
-                                />
-                            ))
-                        }
-                    </Geographies>
-                </ZoomableGroup>
+                {/* <ZoomableGroup> */}
+                <Geographies geography={geoGlobal}>
+                    {({ geographies }) =>
+                        geographies.map((geo) => (
+                            <Geography
+                                key={geo.rsmKey}
+                                geography={geo}
+                                stroke='#EAEAEC'
+                                strokeWidth='0.5'
+                                onMouseEnter={() => {
+                                    const { NAME } = geo.properties;
+                                    countryHovered(`${NAME}`);
+                                }}
+                                onMouseLeave={() => {
+                                    countryHovered('');
+                                }}
+                                onClick={() => onMapClicked(geo.properties)}
+                                fill={
+                                    countrySelected === geo.properties.NAME ? '#668cff' : '#9998A3'
+                                }
+                                style={{
+                                    default: {
+                                        outline: 'none',
+                                    },
+                                    hover: {
+                                        fill: '#809fff',
+                                        stroke: '#3366ff',
+                                        strockWidth: 0.5,
+                                        outline: 'none',
+                                    },
+                                    pressed: {
+                                        fill: '#DD4132',
+                                        stroke: '#9E1030',
+                                        strokeWidth: 0.75,
+                                        outline: 'none',
+                                        transition: 'all 250ms',
+                                    },
+                                }}
+                            />
+                        ))
+                    }
+                </Geographies>
+                {/* </ZoomableGroup> */}
             </ComposableMap>
         </>
     );
